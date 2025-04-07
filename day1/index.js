e414f7990e51cc4ce52400438856a8a0d06c8690
MinaAl5al <minanasser82018@gmail.com>
2025-04-07 09:55:17 +0200

    Let vs Var

--- day1/index.js
+++ day1/index.js
@@ -1,2 +1,14 @@
-console.log(num) // undefined;
-let num = 10; // error
+// console.log(num) // undefined;
+// let num = 10; // error
+
+// console.log(num); // 10
+for (var i = 0; i < 5; i++) {
+  console.log(i); // 0 1 2 3 4
+}
+console.log("i = ",i); // 5
+
+// useing let
+for (let j = 0; j < 5; j++) {
+  console.log(j); // 0 1 2 3 4
+}
+// console.log("j = ",j); // error
