57476c26508a0b71bac2104ca70542f56536db04
MinaAl5al <minanasser82018@gmail.com>
2025-04-07 10:00:27 +0200

    another diff

--- day1/index.js
+++ day1/index.js
@@ -12,3 +12,10 @@ for (let j = 0; j < 5; j++) {
   console.log(j); // 0 1 2 3 4
 }
 // console.log("j = ",j); // error
+
+var a = 10;
+var a = 20; // no error if using let it will give error
+console.log(a); // 20
+
+// let b = 10;
+// let b = 20; // error
