9d2216fa6eede0c3532171e8ffd130c4281bc6db
MinaAl5al <minanasser82018@gmail.com>
2025-04-08 10:09:50 +0200

    day2 Promise

---
+++ day2/index.js
@@ -0,0 +1,91 @@
+// // console.log("first");
+// // setTimeout(function() {
+// //     console.log("second");
+// // }, 1000);
+// // console.log("third");
+
+// function getUsers(cb) {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+//   let users ="Not Found";
+//   xhr.send();
+//   xhr.onload = function () {
+//     if (xhr.status === 200 && xhr.readyState === 4) {
+//       users = JSON.parse(xhr.responseText);
+//       // console.log(data);
+//       cb(users,users[0].id);
+//     }
+//   };
+//   // return users;
+// }
+
+
+// function ShowUsers(users) {
+//   console.log(users);
+// }
+// let  users = getUsers(ShowUsers);
+
+// // GetPosts();
+
+// function getPosts(cb ,userId) {
+//   let xhr = new XMLHttpRequest();
+//   console.log(userId);
+//   xhr.open("GET", "https://jsonplaceholder.typicode.com/posts?userId="+userId+"", true);
+//   let posts = "Not Found";
+//   xhr.send(); 
+//   xhr.onload = function () {
+//     if (xhr.status === 200 && xhr.readyState === 4) {
+//       posts = JSON.parse(xhr.responseText);
+//       // console.log(data);
+//       cb(posts,userId);
+//     }
+//   };
+//   // return posts;
+// }
+
+// function ShowPosts(cb,userId) {
+//   console.log(posts);
+// }
+// let posts = getPosts(ShowPosts);
+
+// // get Comments
+// function getComments(cb,postId) {
+//   let xhr = new XMLHttpRequest();
+//   // let postId = cb.id;
+//   console.log(postId);
+//   xhr.open("GET", "https://jsonplaceholder.typicode.com/comments?postId="+postId+"", true);
+//   let comments = "Not Found";
+//   xhr.send(); 
+//   xhr.onload = function () {
+//     if (xhr.status === 200 && xhr.readyState === 4) {
+//       comments = JSON.parse(xhr.responseText);
+//       // console.log(data);
+//       cb(comments);
+//     }
+//   };
+//   // return posts;
+// } 
+// function ShowComments(postId) {
+//   console.log(comments);
+// }
+
+function getData() 
+{
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhr.send();
+    xhr.onload = function () {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        reject("Error fetching data");
+      }
+    };
+  });
+
+}
+
+getData().then((data) => {
+  console.log(data);
+})
